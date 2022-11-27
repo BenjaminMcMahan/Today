3eac28db22775498cd453fc5bb80507bbcf6a8c1
Benjamin McMahan <108589431+BenjaminMcMahan@users.noreply.github.com>
2022-11-26 21:36:30 -0500

    Persisting data

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import Main from "./screens/Main";
 import { NavigationContainer } from "@react-navigation/native";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 
 export default function App() {
     return (
         <Provider store={store}>
-            <NavigationContainer>
-                <Main/>
-            </NavigationContainer>
+            <PersistGate persistor={persistor}>
+                <NavigationContainer>
+                    <Main/>
+                </NavigationContainer>
+            </PersistGate>
         </Provider>
     )
 }
--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { journalEntriesReducer } from "../features/journal/journalSlice";
+import {
+    persistStore,
+    persistCombineReducers,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const config = {
+    key: 'root',
+    storage: AsyncStorage,
+    debug: true
+};
 
 export const store = configureStore({
-    reducer: {
+    reducer: persistCombineReducers(config, {
         journalEntries: journalEntriesReducer
-    }
+    }),
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER
+                ]
+            }
+        })
 });
+
+export const persistor = persistStore(store);
